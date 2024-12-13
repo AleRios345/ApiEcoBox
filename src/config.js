98024112c3f04f37372a08280e2341f62e8fbc9b
AleRios345 <88628104+AleRios345@users.noreply.github.com>
2024-12-12 18:11:26 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-export const PORT = process.env.PORT || 3000;
-export const DB_USER = process.env.DB_USER || "postgres";
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "Preslopin123#";
-export const DB_DATABASE = process.env.DB_DATABASE || "eco_box_app";
-export const DB_PORT = process.env.DB_PORT || 5432;
-export const SALTROUNDS = process.env.SALTROUNDS || 12;
+export const PORT = process.env.PORT ;
+export const DB_USER = process.env.DB_USER;
+export const DB_HOST = process.env.DB_HOST;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_DATABASE = process.env.DB_DATABASE;
+export const DB_PORT = process.env.DB_PORT;
+export const SALTROUNDS = process.env.SALTROUNDS;
